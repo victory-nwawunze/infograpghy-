1e1d9ea9a32cee7d39e301a2730d5995809eabb5
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-04 10:37:12 -0700

    finish the carousel

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 /** @format */
-function Spinner() {
-  $spinner = document.getElementsByClassName("spinner-grow")[0];
-  //   $spinner.style.display = "block";
-  $spinner.style.marginLeft = 30;
 
-  //   SetTimeout
-  setTimeout(() => {
-    $spinner.style.display = "none";
-  }, 3000);
-}
-// Call the function
-Spinner();
+// /** @format */
+// function Spinner() {
+//   $spinner = document.getElementsByClassName("spinner-border")[0];
+//   //   $spinner.style.display = "block";
+//   $spinner.style.marginLeft = 30;
+
+//   //   SetTimeout
+//   setTimeout(() => {
+//     $spinner.style.display = "none";
+//   }, 3000);
+// }
+// // Call the function
+// Spinner();
